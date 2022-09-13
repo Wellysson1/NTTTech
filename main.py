207878631f5f0bf0805351b0f62d1c939a6270d9
Pedro Lemos <henry-lemos@hotmail.com.br>
2022-09-13 11:23:19 -0300

    🏗️: cryptography parameter
    New logic applied to the parameter, using default value.

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-MODE_ENCRYPT = 1
-MODE_DECRYPT = 0
-
-
-def caesar(data, key, mode):
+def caesar(data, key, encrypted=True):
     alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     new_data = ''
     for c in data:
@@ -10,7 +6,7 @@ def caesar(data, key, mode):
         if index == -1:
             new_data += c
         else:
-            new_index = index + key if mode == MODE_ENCRYPT else index - key
+            new_index = index + key if encrypted else index - key
             new_index = new_index % len(alphabet)
             new_data += alphabet[new_index]
     return new_data
@@ -18,11 +14,11 @@ def caesar(data, key, mode):
 
 key = int(input("Digite a sua chave: "))
 original = input("Digite a sua frase: ")
-ciphered = caesar(original, key, MODE_ENCRYPT)
+ciphered = caesar(original, key)
 print('Encriptada:', ciphered)
 
 decriptar = input("Você deseja decriptar a mensagem? (1)Yes (2)No")
 
 if decriptar == "1":
-    plain = caesar(ciphered, key, MODE_DECRYPT)
+    plain = caesar(ciphered, key, False)
     print('Decriptada:', plain)
