a15a0222b1129575b1fc1c73b48fde009f50c009
Pedro Lemos <henry-lemos@hotmail.com.br>
2022-09-13 11:17:01 -0300

    🗑️: small code cleanup
    - Removed  var chavetxt
    - Slicing simplification
    - Indentation correction

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ def caesar(data, key, mode):
         else:
             new_index = index + key if mode == MODE_ENCRYPT else index - key
             new_index = new_index % len(alphabet)
-            new_data += alphabet[new_index:new_index+1]
+            new_data += alphabet[new_index]
     return new_data
 
-chavetxt = input("Digite a sua chave: ")
-key = int(chavetxt)
+
+key = int(input("Digite a sua chave: "))
 original = input("Digite a sua frase: ")
 ciphered = caesar(original, key, MODE_ENCRYPT)
 print('Encriptada:', ciphered)
@@ -24,5 +24,5 @@ print('Encriptada:', ciphered)
 decriptar = input("Você deseja decriptar a mensagem? (1)Yes (2)No")
 
 if decriptar == "1":
-  plain = caesar(ciphered, key, MODE_DECRYPT)
-  print('Decriptada:', plain)
+    plain = caesar(ciphered, key, MODE_DECRYPT)
+    print('Decriptada:', plain)
