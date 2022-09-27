ddd0d3dcb67104fd23f1876c850052324857c761
Wellysson1 <wellyssonmonteiro@gmail.com>
2022-09-27 12:13:43 -0300

    Update Cifra API

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from ast import Num
-from calendar import c
+from ast import Await, Num
+from itertools import count
 from numbers import Number
 import numbers
 from string import ascii_lowercase
@@ -10,6 +10,7 @@ from urllib import response
 from fastapi import FastAPI
 import random
 from pydantic import BaseModel
+from fastapi import Response
 from json import loads
 
 
@@ -18,15 +19,18 @@ captura = json.loads(response.text)
 frase = captura['facts'][0]
 #print(json_data[0]['res'][0])
 
-MODE_ENCRYPT = 1
-MODE_DECRYPT = 0
-alphabet = 'abcdefghijklmnopqrstuvwyz'
-
 app = FastAPI()
+MODE_ENCRYPT = 1
+alphabet = 'abcdefghijklmnopqrstuvwyz'
+key = random.randrange(1,24)
+
+class Item(BaseModel):
+    frase = str
+
 @app.get("/getCifra")
-def codifica(data, key, mode):
+def codifica(frase, key, mode):
     new_data = ''
-    for c in data:
+    for c in frase:
         index = alphabet.find(c)
         if index == -1:
             new_data += c
@@ -34,26 +38,38 @@ def codifica(data, key, mode):
             new_index = index + key if mode == MODE_ENCRYPT else print("Erro")
             new_index = new_index % len(alphabet)
             new_data += alphabet[new_index:new_index+1]
-    return {"Frase": new_data, "Chave": key}
+    return {"Frase": new_data, "chave": key}
 
-key = random.randrange(1,24)
+
 original = frase
-ciphered = codifica(original, key, MODE_ENCRYPT)
+ciphered = codifica(frase, key, MODE_ENCRYPT)
 class json():
     def __init__(self, frase, key):
         self.ciphered = frase
         self.key = key
         data = json.dumps(json.__dict__)
     
-print('Encriptada:', ciphered)
+print('Encriptada:', ciphered) 
+
+class Itemm(BaseModel):
+    frase = str
+    key = int
 
 @app.post("/resolveCifra")
-def decodifica(frase, key):
+def decodifica(itemm:Itemm):
     new_data = ''
     for c in frase:
-        index = ((ord(c) - ord('a')) + 26 - key) %26
-        new_data += alphabet[index]
+        index = alphabet.find(c)
+        if index == -1:
+            new_data += c
+        else:
+            new_index = index - int(key)
+            new_index = new_index % len(alphabet) %27
+            new_data += alphabet[new_index:new_index+1]
     return {"Mensagem Decifrada": new_data}
+    
+    
+
 
 
     
