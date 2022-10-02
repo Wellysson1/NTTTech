8b93c41199b81b237f4d7873ef55b41a1a4d363b
Wellysson1 <wellyssonmonteiro@gmail.com>
2022-10-02 19:12:07 -0300

    Adicionando metodos para bter raças e frases
    Adicionado os métodos getFacts e getRaca, assim como os dados de acesso ao BD.

--- main.py
+++ main.py
@@ -3,10 +3,72 @@ import random
 import requests
 from fastapi import FastAPI
 from pydantic import BaseModel
+import mysql.connector
 
+mydb = mysql.connector.connect(
+    user="root", 
+    password="Wc24031998",
+    host="127.0.0.1"
+    )
+mycursor = mydb.cursor()
+mycursor.execute("create database if not exists NTTechGrupo6;")
+mycursor.execute("USE NTTechGrupo6;")
+mycursor.execute("CREATE TABLE IF NOT EXISTS `nttechgrupo6`.`facts` (`Frase` VARCHAR(255) NOT NULL, UNIQUE INDEX `Frase_UNIQUE` (`Frase` ASC));;")
+mycursor.execute("CREATE TABLE IF NOT EXISTS`nttechgrupo6`.`breed` (`Id_Breed` INT NOT NULL AUTO_INCREMENT, `Raca` VARCHAR(100) NOT NULL, PRIMARY KEY (`Id_Breed`), UNIQUE INDEX `Id_Breed_UNIQUE` (`Id_Breed` ASC) VISIBLE, UNIQUE INDEX `Raca_UNIQUE` (`Raca` ASC) VISIBLE);")
 
-MODE_ENCRYPT = 1
+captura: list = []
+lista_racas = []
+
+for item in lista_racas:
+    print(lista_racas)
+
 app = FastAPI()
+
+@app.get("/getRaca")
+def insertBreed():
+    mydb = mysql.connector.connect(
+    user="root", 
+    password="Wc24031998",
+    host="127.0.0.1",
+    database="nttechgrupo6"
+    )
+    mycursor = mydb.cursor()
+    #mycursor.execute("INSERT IGNORE INTO breed (Raca) VALUES("Dog")")
+    mycursor.commit()
+    index = 0
+    while (index < 173):
+        response = requests.get("https://api.thedogapi.com/v1/breeds?&page=0")
+        captura = json.loads(response.content)
+        item = captura[index]['name']
+        mycursor.execute("INSERT IGNORE INTO breed (Raca) VALUES(%s)", (item, ))
+        mydb.commit()
+        print(mycursor)
+        index = index + 1        
+    mydb.close()
+    
+    
+@app.get("/getFacts")
+def insertBreed():
+    mydb = mysql.connector.connect(
+    user="root", 
+    password="Wc24031998",
+    host="127.0.0.1",
+    database="nttechgrupo6"
+    )
+    mycursor = mydb.cursor()
+    index = 0
+    while (index < 100):
+        response = requests.get("https://dog-api.kinduff.com/api/facts")
+        captura = json.loads(response.content)
+        item = captura['facts'][0]
+        mycursor.execute("INSERT IGNORE INTO facts (Frase) VALUES(%s)", (item, ))
+        mydb.commit()
+        print(mycursor)
+        index = index + 1        
+    mydb.close()
+              
+
+
 alphabet = 'abcdefghijklmnopqrstuvwyz'
     
 class Itemm(BaseModel):
@@ -19,12 +81,6 @@ def codifica():
     captura = json.loads(response.content)
     frase = captura['facts'][0]
     key = random.randrange(1,24)
-    class jsson():
-        def __init__(self, frase):
-            self.ciphered = frase
-            self.key = key
-            data = json.dumps(json.__dict__)
-            ciphered = codifica(frase, key, MODE_ENCRYPT)
     new_data = ''
     for c in frase:
         index = alphabet.find(c)
